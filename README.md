843ae0809fedab3ecf79bc87c6ffd289cc218960
Shubham Hibare <hibare@users.noreply.github.com>
2017-09-27 18:45:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-# redis-installation-script
-This script helps to install REDIS, a key value pair NoSQL database. This script is intended for the persons who just want to start with the REDIS without going through tedious installation procedure.
+=================================================================================
++                     Install / uninstall Redis on Ubuntu                       +
+=================================================================================
+                       mmm  mmmmm          mmmm m     m
+                     m"   " #   "#  m""#m "   "# "m m" 
+                     #      #mmmm" #  mm#     m"  "#"  
+                     #      #   "m # #  #   m"     #   
+                      "mmm" #    " #m """ m#mmmm   #   
+                                    "mmm               
+                
+                    Developed by : Shubham Hibare (CR@2Y)
+                    website      : http://hibare.in
+                    Linkedin     : https://linkedin.com/in/hibare
+                    Github       : https://hibare.github.io
+                    License      : Apache License 2.0
+=================================================================================
 
-To install the REDIS, run command "sh install.sh" (without quotes) with superuser access.
+What it is?
+This project contains shell script that automates the process of installation and uninstallation of Redis on Ubuntu. These scripts are tested on UBUNTU 16.04 LTS and works perfectly. These scripts are expected to work any version of UBUNTU.
+
+Whom it is for?
+These scripts are intended for the users who wants to get ride of the installation and uninstallation process of Redis and directly work on it.
+
+How to use this script?
+Installation :-
+Note: Working internet connection is required during installation process. Super user (root) access is required during installation and uninstallation process.
+
+Step 1: Clone this repository and extract. After cloning, you'll have these three files in the directory: install.sh, uninstall.sh and README.md. 
+
+Step 2: Open a terminal and navigate to the directory.
+
+Step 3: Run the command "sh install.sh" (Without quoted). Installation script will automatically downloads the latest stable version of Redis and install it.
+
+Step 4:Use redis-server command to start the server and redis-cli command to open a client.
+
+Uninstallation :-
+Step 1: Open a terminal and navigate to the directory.
+
+Step 2: run command "sh uninstall.sh" (without quotes).
+
+Disclaimer: Author of these scripts is not responsible for any damage caused to your system from using these scripts. Use these scripts at your own risk.
+
+Note: For any errors and queries contact from here- http://hibare.in.
--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-#Redis automatic installation script
+#Uninstallation script
+
+#Clear terminal screen
+clear
 
 #Declaring color variables
 CYAN='\033[0;36m'       #CYAN COLOUR
 NC='\033[0m'            #NO COLOUR
 GREEN='\033[0;32m'      #GREEN COLOUR
 RED='\033[0;31m'        #RED COLOUR
-YELLOW='\033[1;33m'	#YELLOW COLOUR
-
-
-#Clear terminal screen
-clear
+YELLOW='\033[1;33m'
 
 #Banner
 echo "======================================================"
-echo "+                   Install redis                    +"
-echo "======================================================"
+echo "+                 uninstall redis                    +"
+echo "======================================================"                                                  
 echo "    [0;1;34;94m▄▄▄▄[0m   [0;34m▄▄▄▄▄▄[0m               [0;37m▄▄▄▄▄[0m   [0;1;30;90m▄▄▄[0m    [0;1;30;90m▄▄▄[0m"
 echo "  [0;34m██▀▀▀▀█[0m  [0;34m██▀▀▀[0;37m▀██[0m   [0;37m▄████▄[0m   [0;37m█[0;1;30;90m▀▀▀▀██▄[0m  [0;1;30;90m██▄[0m  [0;1;30;90m▄█[0;1;34;94m█[0m" 
 echo " [0;34m██▀[0m       [0;37m██[0m    [0;37m██[0m [0;37m▄██▀[0m  [0;1;30;90m▀██[0m        [0;1;30;90m██[0m   [0;1;34;94m██▄▄██[0m " 
@@ -35,8 +34,8 @@ echo "  License      : Unlicensed"
 echo "======================================================"
 echo "${RED}Disclaimer:${NC} Author of this script is not responsible 
 for any damage caused to your system from using this 
-script. Use this script at your own risk. You have ${RED}15 
-seconds${NC} to abort the process."
+script. Use this script at your own risk. You have 15 
+seconds to abort the process"
 echo "======================================================"
 sleep 15
 
@@ -46,94 +45,27 @@ if [ `id -u` != 0 ]; then
 	exit 1
 fi
 
-echo "${CYAN}Installing redis ... ${nc}"
-
-#Download latest version of redis
-echo -n  "${YELLOW}[*] Downloading redis ... ${NC}"
-wget http://download.redis.io/releases/redis-stable.tar.gz > /dev/null 2>&1
-ls -l redis-stable.tar.gz > /dev/null 2>&1
+#Remove links
+echo -n "[*] Removing links ... "
+rm /bin/redis-server > /dev/null 2>&1
+rm /bin/redis-cli > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "${GREEN}[Done]${NC}"
 else
 	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
+	echo "${RED}Abort{$NC}"
 	exit
 fi
 
-#Create file structure
-echo -n "${YELLOW}[*] Creating file structure ... ${NC}"
-mkdir -p ~/.crazyScripts
+#Remove program
+echo -n "[*] Removing program ... "
+rm -r ~/.crazyScripts/redis-stable > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	echo "${GREEN}[Done]${NC}"
 else
-	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
-	exit
+        echo "${RED}[Failed]${NC}"
+        echo "${RED}Abort{$NC}"
+        exit
 fi
+echo "${GREEN} Completed. ${NC}"
 
-#Copy redis file
-echo -n  "${YELLOW}[*] Copying redis file ...${NC}"
-cp -f redis-stable.tar.gz ~/.crazyScripts > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "${GREEN}[Done]${NC}"
-else
-	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
-	exit
-fi
-
-#Chage pwd
-cd ~/.crazyScripts
-
-#Extract files
-echo -n "${YELLOW}[*] Extracting files ... ${NC}"
-tar xzf redis-stable.tar.gz > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "${GREEN}[Done]${NC}"
-else
-	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
-	exit
-fi
-
-#Remove file
-rm -f redis-stable.tar.gz > /dev/null 2>&1
-
-#Change PWD to redis
-cd redis-stable
-
-#Make files
-echo -n "${YELLOW}[*] Executing make ... ${NC}"
-make > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "${GREEN}[Done]${NC}"
-else
-	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
-	exit
-fi
-
-
-#Make test
-echo -n "${YELLOW}[*] Executing make test ... ${NC}"
-make test > /dev/null 2>&1
-echo "${GREEN}[Done]${NC}"
-
-cd ~
-
-#Create links
-echo -n "${YELLOW}[*] Creating links ... ${NC}"
-home=`pwd`
-ln -s $home/.crazyScripts/redis-stable/src/redis-server /bin/redis-server > /dev/null 2>&1
-ln -s $home/.crazyScripts/redis-stable/src/redis-cli /bin/redis-cli > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	echo "${GREEN}[Done]${NC}"
-else
-	echo "${RED}[Failed]${NC}"
-	echo "${RED}ABORT${NC}"
-	exit
-fi
-echo "${CYAN}Completed...${NC}"
-echo "${YELLOW}Important to read: ${NC}"
-echo "To start server use command redis-server"
-echo "To get client use command redis-cli"
